548cadfd076d804591013c6a61241c1b96b3f29e
ilies <iliesmraihia@gmail.com>
2025-01-04 17:19:30 +0100

    link

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,5 +1,5 @@
 export function fetchBackend(url, options) {
-    return fetch("http://tiptalk.up.railway.app" + url, {
+    return fetch("https://tiptalk.up.railway.app" + url, {
         ...options, // spread the existing options
         credentials: 'include', // Ensure cookies are sent with the request
     });

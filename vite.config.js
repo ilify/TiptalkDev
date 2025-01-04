6e9cc62c27c703a80c5c15dd2a463f2dac640f32
ilies <iliesmraihia@gmail.com>
2025-01-04 18:01:59 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,6 @@ export default defineConfig({
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-		},
 	},
 	resolve: {
 		alias: {
